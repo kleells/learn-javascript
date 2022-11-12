20dbadb615a098899ffe92e37564bd00df753f20
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-12 16:29:47 -0500

    practiced functions with countdown()

--- 1. Build a Passenger Counter App/9. Using functions to write less code/index.js
+++ 1. Build a Passenger Counter App/9. Using functions to write less code/index.js
@@ -1,19 +1,37 @@
-// Setting up the the race 🏎 🏎 🏎
+// Practice to build understanding of functions by building a racecar game
 
-console.log(5)
-console.log(4)
-console.log(3)
-console.log(2)
-console.log(1)
+// Setting up the the race 🏎 🏎 🏎 with a countdown
+
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
 
 // GO! 🏁
 // Players are running the race 🏎 💨
 // Race is finished! 🍾
 
-// Get ready for a new race 🏎 🏎 🏎
+// Get ready for a new race 🏎 🏎 🏎 with a countdown
 
-console.log(5)
-console.log(4)
-console.log(3)
-console.log(2)
-console.log(1)
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+
+// There is a way to make this less repetitive vvvv
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+// invoke the function 
+
+countdown()
+// countdown() is called whenver you want to start a new race
+// (instead of writing out all of those console logs)
