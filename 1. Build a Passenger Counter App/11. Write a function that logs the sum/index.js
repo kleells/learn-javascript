e52ab99515eb441de52b3702d2ec3a961ce4d974
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-12 17:41:56 -0500

    created lapIncrement function

--- 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
+++ 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
@@ -4,3 +4,17 @@ let lap3 = 36
 
 // Create a function that logs out the sum of all the lap times
 
+// 1st way
+function totalRaceTime() {
+    console.log(lap1 + lap2 + lap3)
+}
+
+totalRaceTime()
+
+// 2nd way
+function raceTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+}
+
+raceTime()
--- 1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -1,9 +1,13 @@
 let lapsCompleted = 0
 
 // Create a function that increments the lapsCompleted variable with one
+function lapIncrement() {
+    lapsCompleted = lapsCompleted + 1
+}
 // Run it three times
 
-
-
+lapIncrement()
+lapIncrement()
+lapIncrement()
 
 console.log(lapsCompleted)
