359cc320c70e7f94a78439d60cbaf3ebf2084d22
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-13 15:05:00 -0500

    change the count-el in the HTML to reflect the new count

--- 1. Build a Passenger Counter App/14. Display the count/index.js
+++ 1. Build a Passenger Counter App/14. Display the count/index.js
@@ -1,12 +1,17 @@
-// document.getElementById("count").innerText = 5
+let countEl = document.getElementById("count-el")
 
-// change the count-el in the HTML to reflect the new count
+console.log(countEl)
 
+// intialize the count as 0
 let count = 0
 
+// listen for clicks on the increment button
 function increment() {
+    console.log('clicked')
+    // increment the count variable when the button is clicked
     count = count + 1
+    // change the count-el in the HTML to reflect the new count
+        // Starts with creating countEl variable in line 1
+    countEl.innerText = count
     console.log(count)
 }
-
-
