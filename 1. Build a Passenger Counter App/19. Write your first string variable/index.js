f1f7ddb3d4aeebddb170767ef4ca44769433bf7e
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-14 20:28:13 -0500

    string practice

--- 1. Build a Passenger Counter App/19. Write your first string variable/index.js
+++ 1. Build a Passenger Counter App/19. Write your first string variable/index.js
@@ -1,4 +1,11 @@
-let username = 'per"
+let username = 'per'
 
-console.log(username)
+// Create a variable, message, that stores the string: "You have three new notifications"
 
+let message = 'You have three new notifications'
+
+// Create a variable, messageToUser, that contains the message we have logged
+
+let messageToUser = message + ', ' + username + '!'
+
+console.log(messageToUser)
