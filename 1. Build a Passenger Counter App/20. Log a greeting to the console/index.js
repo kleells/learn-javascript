df21934c8440f015be02d43c175fe78f02d90eeb
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-15 19:33:19 -0500

    creating myGreeting variable

--- 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
+++ 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
@@ -1,7 +1,16 @@
 // Create two variables, name and greeting. The name variable should store your name,
 // and the greeting should store e.g. "Hi, my name is "
 
+let myName = "Katie Eells"
+
+let greeting = "Hi, my name is "
+
 // Create a third variable, myGreeting, that contatenates the two strings
 // Log myGreeting to the console
 
+let myGreeting = greeting + myName
+
+console.log(myName)
+console.log(greeting)
+console.log(myGreeting)
 
