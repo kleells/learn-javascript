a48dc23b208602f5c567cbcd99007c27c0d36381
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-20 13:53:45 -0500

    added saveEl variable and save()

--- 1. Build a Passenger Counter App/24. Use plus equal for count/index.js
+++ 1. Build a Passenger Counter App/24. Use plus equal for count/index.js
@@ -3,7 +3,8 @@ let count = 0
 
 function increment() {
     // Change this to use the plus equal technique you've learned
-    count = count + 1
+    // count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
