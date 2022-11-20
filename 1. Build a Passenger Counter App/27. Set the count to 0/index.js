ca9cd3383e2d4abf147df6d453df79550e5025ce
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-20 14:40:53 -0500

    set counter back to zero after save

--- 1. Build a Passenger Counter App/27. Set the count to 0/index.js
+++ 1. Build a Passenger Counter App/27. Set the count to 0/index.js
@@ -10,5 +10,9 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    // set the count back to 0
+    countEl.textContent = 0
+    // set the count to zero or the count with keep adding +1 from last save click
+    count = 0
 }
 
