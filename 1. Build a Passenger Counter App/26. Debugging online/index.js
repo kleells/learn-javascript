9c378aacfa52b263a039d6ae2038eb9e949a1040
Katie Eells <katieeells@Katies-MBP.lan>
2022-11-20 14:31:18 -0500

    changed .innerText to .textContent

--- 1. Build a Passenger Counter App/26. Debugging online/index.js
+++ 1. Build a Passenger Counter App/26. Debugging online/index.js
@@ -4,11 +4,13 @@ let count = 0
 
 function increment() {
     count += 1
+    // countEl.innerText = count <-- like line 13-14, this is changed to utilize .textContent
     countEl.textContent = count
 }
 
 function save() {
     let countStr = count + " - "
+    // saveEl.innerText += countStr <-- this can be changed to a .textContent to show spaces added to countStr string and render prettier
     saveEl.textContent += countStr
 }
 
